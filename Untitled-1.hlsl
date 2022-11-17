cfd56ec53ae739836cb67662389ab870a8da246f
laurin61 <97901665+laurin61@users.noreply.github.com>
2022-11-17 17:05:38 +0300

    jhkh
    hghgh

---
+++ Untitled-1.hlsl
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script type="text/javascript">
+function karekok() {
+var x=prompt("lütfen sayıyı giriniz","buraya giriniz");
+var y=Math.sqrt(x);
+alert(x+" sayısının karekökü:  " +y);
+
+
+}
+
+</script>
+
+</head>
+<body>
+<form>
+<input   type="button" value="karekök" onclick="karekok();"/> 
+
+</form>    
+</body>
